0dcaed075cfe8d861ab7f05cf4385a77fb38cd79
MarcusNCo <levert.marc.a@gmail.com>
2022-02-04 16:25:19 +0100

    MàJ BG modale

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,15 @@
 test.addEventListener('click', () => {
     myModal.style.display = 'flex';
+    myModalBg.style.display = 'block';
 });
 
 closeModalBuy.addEventListener('click', () => {
-    myModal.style.display = 'none'
+    myModal.style.display = 'none';
+    myModalBg.style.display = 'none';
 });
+
+window.onclick = function(event) {
+    if (event.target == myModalBg) {
+        myModalBg.style.display = "none";
+    }
+}
