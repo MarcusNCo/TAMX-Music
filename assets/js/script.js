16bff4839d654cffd439fcdef4d8146adbdabd0d
Xavier <95688089+xavierfoulon@users.noreply.github.com>
2022-02-07 11:29:25 +0100

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+const navigation = document.querySelector('nav')
+
+window.addEventListener('scroll', () => {
+
+    if (window.scrollY > 500){
+        navigation.classList.add('anim-nav')
+    } else {
+        navigation.classList.remove('anim-nav')
+    }
+})
