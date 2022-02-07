1486c9b7ce8dfbf45e2144935d7edf16c1e8c013
Thomas Guillemont <thomas.guillemont@hotmail.fr>
2022-02-07 17:05:28 +0100

    JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,12 @@
+let allProducts = [];
+
 fetch('./assets/data/instruments.json') // aller chercher le json
     .then((response) => { // contenu stocker dans response
         return response.json(); // retourne ce qu'il a trouver
     })
     .then((datas) => {
         // var datasProduct = datas; // Ce que on recupere on le met dans datasProduct (pour le panier)
-
+        allProducts = datas;
         // automatise l'ensemble de l'objet datas pour récuperer ses propriétés et permettre de ne plus être limiter si on ajoute une catégorie
         for (const property in datas) {
             datas[property].forEach((element) => { 
@@ -34,7 +36,6 @@ fetch('./assets/data/instruments.json') // aller chercher le json
         // variable button ajouter au panier
         let productItem = document.querySelectorAll('.productItem');
 
-
         // Button clic productItem
         productItem.forEach(element => {
             element.addEventListener('click', (event) => {
@@ -57,90 +58,113 @@ fetch('./assets/data/instruments.json') // aller chercher le json
                         [event.target.getAttribute('id'), 1]
                     ])); // on push un panier
                 }
-                myModal.style.display = 'flex';
-                myModalBg.style.display = 'block';
+                
             })
         });
 
-        const displayCart = (itemJson) => {
-            // cartContainer.innerHTML += `${itemJson.original_title} <img src="${itemJson.product_poster}">`;
-            cartImg.innerHTML +=
-                `
-                <div>${itemJson.original_title}</div>
-                <img class="imgModal m-3" src="${itemJson.product_poster}" alt="image produit">
-                `
-            ;
-        }
-
-        // affichage des elements du panier
-        let cart = JSON.parse(localStorage.getItem('cart'));
-
-        cart.forEach(itemCart => {
-            datas.clavier.forEach(itemJson => {
-                if (itemCart[0] == itemJson.id) {
-                    displayCart(itemJson);
-                    // affichage des quantités du panier
-                    cartImg.innerHTML +=
-                    `
-                    <div>${itemCart[1]}</div>
-                    `;
-                }
-            });
-            datas.boiteRythme.forEach(itemJson => {
-                if (itemCart[0] == itemJson.id) {
-                    displayCart(itemJson);
-                    // affichage des quantités du panier
-                    cartImg.innerHTML +=
-                    `
-                    <div>${itemCart[1]}</div>
-                    `;
-                }
-            });
-            datas.batterie.forEach(itemJson => {
-                if (itemCart[0] == itemJson.id) {
-                    displayCart(itemJson);
-                    // affichage des quantités du panier
-                    cartImg.innerHTML +=
-                    `
-                    <div>${itemCart[1]}</div>
-                    `;
-                }
-            });
-            datas.guitar.forEach(itemJson => {
-                if (itemCart[0] == itemJson.id) {
-                    displayCart(itemJson);
-                    // affichage des quantités du panier
-                    cartImg.innerHTML +=
-                    `
-                    <div>${itemCart[1]}</div>
-                    `;
-                }
-            });
-            datas.studio.forEach(itemJson => {
-                if (itemCart[0] == itemJson.id) {
-                    displayCart(itemJson);
-                    // affichage des quantités du panier
-                    cartImg.innerHTML +=
-                    `
-                    <div>${itemCart[1]}</div>
-                    `;
-                }
-            });
-        });
     })
+const displayCart = (itemJson) => {
+    product.innerHTML +=
+    `
+    <div>
+        <div class="priceTitle w-100 d-flex justify-content-between">
+        <h4 class="productPrice">${itemJson.original_title}</h4>
+        <h4 class="productTitle">${itemJson.product_price} </h4>
+    </div>
+    <img class="imgModal m-3" src="${itemJson.product_poster}" alt="">
+    <div class="quantityDeleteValid w-100 d-flex justify-content-around">
+        <div class="blockQuantityDelete w-100 h-50">
+            <div class="input-group w-25 mb-3">
+                <button class="btn btn-dark dropdown-toggle btnQuantity" type="button"
+                    data-bs-toggle="dropdown" aria-expanded="false">Qté</button>
+                <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" id="1" href="#">1</a></li>
+                    <li><a class="dropdown-item" id="2" href="#">2</a></li>
+                    <li><a class="dropdown-item" id="3" href="#">3</a></li>
+                    <li><a class="dropdown-item" id="4" href="#">4</a></li>
+                    <li><a class="dropdown-item" id="5" href="#">5</a></li>
+                    <li><a class="dropdown-item" id="6" href="#">6</a></li>
+                    <li><a class="dropdown-item" id="7" href="#">7</a></li>
+                    <li><a class="dropdown-item" id="8" href="#">8</a></li>
+                    <li><a class="dropdown-item" id="9" href="#">9</a></li>
+                    <li><a class="dropdown-item" id="10" href="#">10</a></li>
+                </ul>
+            </div>
+            <img class="pictureBin" id="binBtn" src="assets/img/Bin (1).png" alt="corbeille">
+        </div>
+    </div>
+    `
+}        
+// affichage des elements du panier
+const loadCart = (datas) => {
+    let cart = JSON.parse(localStorage.getItem('cart'));
 
-
+    let totalHt = 0;
+    let totalTtc = 0;
+    product.innerHTML = "";
+    
+    cart.forEach(itemCart => {
+        datas.clavier.forEach(itemJson => {
+            if (itemCart[0] == itemJson.id) {
+                console.log(cart);
+                displayCart(itemJson);
+                let price = itemJson.product_price.split(' ')[0];
+                totalHt = (totalHt + Number(price)) * itemCart[1];
+                totalTtc = totalHt * 1.20;
+            }
+        });
+        datas.boiteRythme.forEach(itemJson => {
+            if (itemCart[0] == itemJson.id) {
+                displayCart(itemJson);
+                let price = itemJson.product_price.split(' ')[0];
+                totalHt = totalHt + Number(price);
+                totalTtc = totalHt * 1.20;
+            }
+        });
+        datas.batterie.forEach(itemJson => {
+            if (itemCart[0] == itemJson.id) {
+                displayCart(itemJson);
+                let price = itemJson.product_price.split(' ')[0];
+                totalHt = totalHt + Number(price);
+                totalTtc = totalHt * 1.20;
+            }
+        });
+        datas.guitar.forEach(itemJson => {
+            if (itemCart[0] == itemJson.id) {
+                displayCart(itemJson);
+                let price = itemJson.product_price.split(' ')[0];
+                totalHt = totalHt + Number(price);
+                totalTtc = totalHt * 1.20;
+            }
+        });
+        datas.studio.forEach(itemJson => {
+            if (itemCart[0] == itemJson.id) {
+                displayCart(itemJson);
+                let price = itemJson.product_price.split(' ')[0];
+                totalHt = totalHt + Number(price);
+                totalTtc = totalHt * 1.20;
+            }
+        });
+        totalProductHt = totalHt.toFixed(2);
+        totalProductTtc = totalTtc.toFixed(2);
+        resultHT.innerHTML = `${totalProductHt} €`;
+        resultTTC.innerHTML = `${totalProductTtc} €`;
+    });
+    
+}
 // Button reset panier
 resetCart.addEventListener('click', () => {
     // supprimer localStorage panier
     localStorage.removeItem('cart');
+    // reinitialisation valeurs calc
+    product.innerHTML = "";
+    resultHT.innerHTML = "0 €";
+    resultTTC.innerHTML = "0 €";
 })
 
-
 // modal
-let productItem = document.querySelectorAll('.productItem');
-
-test.addEventListener('click', () => {
+openModal.addEventListener('click', () => {
+    loadCart(allProducts);
     myModal.style.display = 'flex';
     myModalBg.style.display = 'block';
 });
@@ -153,14 +177,3 @@ window.onclick = function(event) {
         myModalBg.style.display = "none";
     }
 }
-
-const navigation = document.querySelector('nav')
-
-window.addEventListener('scroll', () => {
-
-    if (window.scrollY > 500){
-        navigation.classList.add('anim-nav')
-    } else {
-        navigation.classList.remove('anim-nav')
-    }
-})
